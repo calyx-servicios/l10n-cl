2a90082315fae9c320dcb810d3b7337d05927aa8
Fede Gregori <jfedericogregori@gmail.com>
2022-04-19 14:41:09 -0300

    [FIX] Import and exento method.

--- cx_l10n_cl_fe_exento/models/account_move.py
+++ cx_l10n_cl_fe_exento/models/account_move.py
@@ -5,11 +5,9 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def exento(self):
-        exento = sum(
-            [
-                l.price_subtotal
-                for l in self.mapped("invoice_line_ids")
-                if l.tax_ids.amount == 0
-            ]
-        )
-        return exento if exento else (exento * -1)
+        exento = 0
+        for l in self.mapped("invoice_line_ids"):
+            for _ in l.mapped("tax_ids").filtered(lambda t: t.amount == 0):
+                exento += l.price_subtotal
+                break
+        return exento if exento > 0 else (exento * -1)
