38fbf2713f0246d5d92619c6cbedff63e9d7688b
gpperez <gpperez@gmail.com>
2022-09-06 17:41:53 -0300

    [ADD][l10n_cl_edi_fix_multicompany] Incidence nbr./s.:ODOO-4350 Migrar Módulos Boleta Electrónica - Bitex - This module fixes upload CAF files in multicompany environment.

---
+++ l10n_cl_edi_fix_multicompany/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Chile - E-invoicing (Fix multicompany CAF DTE)",
+    "summary": """
+        This module fixes upload CAF files in multicompany environment.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["gpperez"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Localization/Chile",
+    "version": "13.0.1.0.0",
+    "installable": True,
+    "application": False,
+    "depends": ["l10n_cl_edi",],
+}
